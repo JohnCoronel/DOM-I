19edebb724d71107ae4d40b30413939ae634599a
John Coronel <john.m.coronel@gmail.com>
2018-03-05 21:15:21 -0800

    Seconds Timer update

--- digital_timer/index.js
+++ digital_timer/index.js
@@ -3,31 +3,32 @@
 
 
 let current = 0;
+let currentSec = 0;
 
 const secondOnes = window.setInterval( () => {
-    secondOnes = document.getElementById('secondOnes');
-    secondTime = document.getElementById('secondTens');
+    secondOne = document.getElementById('secondOnes');
+    secondTens = document.getElementById('secondTens');
 
-    secondTime.innerHTML = 0;
-    secondOnes.innerHTML = current;
+    secondOne.innerHTML = current;
     current = ++current;
   
         
     if (current === 10) {
         current = 0;
+        currentSec = 0;
     }
 }, 1000);
 
 
 
-let currentSec = 0;
+
 const secondTimes = window.setInterval( () => {
     secondTime = document.getElementById('secondTens');
-    secondTime.innerHTML = currentSec;
+    
     if ( currentSec === 0) {
         currentSec = ++currentSec;
     }
-},10000);
+}, 10000);
 
 
 
